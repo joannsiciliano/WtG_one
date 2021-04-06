a69a0e5131b8a1265017029f06d4c0d007c8be08
Alexander Sherwin <alx.sherwin@gmail.com>
2021-04-06 13:02:10 -0600

    final commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const toggle = document.getElementById('toggle')
+
+// toggle.addEventListener("click", function() {
+
+// })
+
+toggle.onclick = function() {
+    toggle.classList.toggle('active')
+}
